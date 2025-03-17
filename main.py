acdebdddb899bd75f76347069384c384590b0741
Bashar Saeed <bwasaeed@gmail.com>
2025-03-17 23:26:38 +0300

    render player on screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import pygame
 import constants
+from player import Player
 
 
 def main() -> None:
@@ -8,6 +9,7 @@ def main() -> None:
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    player = Player(constants.SCREEN_WIDTH // 2, constants.SCREEN_HEIGHT // 2)
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,7 @@ def main() -> None:
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
